e85ca2a6f5196612da0926f563300c6fc4d7706e
ZackEnixey <zzftn90@gmail.com>
2021-01-26 18:42:03 +0100

    I fetched the book's material

--- src/pages/Graph.js
+++ src/pages/Graph.js
@@ -1,13 +1,32 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect} from 'react';
+import bookURL from './book.txt';
+
+
+
 
 export default class Graph extends Component {
+   
+    constructor(){
+        super();
+        this.state = {
+            bookContent: "this is initial state"
+        };
+    }
+    componentWillMount(){
+        fetch(bookURL).then( bookData => {
+            return bookData.text();
+        }).then( bookData => {
+            console.log(bookData);
+            this.setState({ bookContent: bookData} );
+        })
+    };
+
 
     render(){
         return(
             <div> 
-
+                <div> {this.state.bookContent} </div>
                 graph se ovde vidi
-
             </div>
         )
     }
